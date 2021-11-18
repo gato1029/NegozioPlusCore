a2bdd71526892d70c0fd8adc11d4727ac10f0225
josue <josue.ccama@gmail.com>
2021-11-17 22:26:48 -0500

    Se agrega ventana de usuario interfaz
    Se crea nuevo control switch button, se cambia todo a depencia stylizada y probando nuevas fuentes

---
+++ NegozioPlusCore/MVVM/Usuarios/VM/UsuarioVentanaVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegozioPlusCore.MVVM.Usuarios.VM
+{
+    class UsuarioVentanaVM
+    {
+        private ObservableCollection<ModulosUsuario> checkListModulos;
+
+        public UsuarioVentanaVM()
+        {
+            CheckListModulos = new ObservableCollection<ModulosUsuario>();
+            checkListModulos.Add(new ModulosUsuario("demo"));
+            checkListModulos.Add(new ModulosUsuario("demo2"));
+            checkListModulos.Add(new ModulosUsuario("demo3"));
+            checkListModulos.Add(new ModulosUsuario("demo4"));
+        }
+
+        public ObservableCollection<ModulosUsuario> CheckListModulos { get => checkListModulos; set => checkListModulos = value; }
+    }
+
+    public class ModulosUsuario
+    {
+        public ModulosUsuario(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public string Nombre { get; set; }
+    }
+}
---
+++ NegozioPlusCore/NucleoRealm/Modelos/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using Realms;
+
+namespace NegozioPlusCore.NucleoRealm.Modelos
+{
+    public class Usuario : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        public ObjectId? Id { get; set; }
+        [MapTo("docIdentidad")]
+        public string DocIdentidad { get; set; }
+        [MapTo("idEmp")]
+        public string IdEmp { get; set; }
+        [MapTo("idUsuario")]
+        public string IdUsuario { get; set; }
+        [MapTo("particion")]
+        public string Particion { get; set; }
+        [MapTo("rol")]
+        public string Rol { get; set; }
+        [MapTo("tipoDoc")]
+        public string TipoDoc { get; set; }
+    }
+}
