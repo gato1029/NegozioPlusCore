214eef9dcbbd46e9a805b04432b539fbf4bdca55
Kenny <kennymorocco@gmail.com>
2021-11-17 21:00:23 -0500

    Actualización de vistas
    Se actualizo las vistas de productos, almacén, tienda y categorías al nuevo estilo que se implemento en la rama gato.

---
+++ NegozioPlusCore/MVVM/Tiendas/VM/TiendasVM.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using NegozioPlusCore.MVVM.Principal;
+using NegozioPlusCore.MVVM.Principal.VM;
+using NegozioPlusCore.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace NegozioPlusCore.MVVM.Tiendas.VM
+{
+    class TiendasVM : NotificadorGenerico
+    {
+        //private UsuarioVentana usuarioVentanaNuevo;
+        private double altoGridDatos;
+        public ObservableCollection<TiendasXAML> Coleccion { get; set; }
+        public ICommand ComandoClickAgregar => new RelayCommand<Object>(ClickAgregar, (o) => { return true; });
+
+        private void ClickAgregar(object obj)
+        {
+            /*
+            if (usuarioVentanaNuevo == null || usuarioVentanaNuevo.IsClosed)
+            {
+                usuarioVentanaNuevo = new UsuarioVentana();
+            }
+            usuarioVentanaNuevo.Show();
+            */
+        }
+
+        public TiendasVM()
+        {
+            PrincipalPaginaVM.EventoResizarVentana += PrincipalPaginaVM_EventoResizarVentana;
+            Coleccion = new ObservableCollection<TiendasXAML>();
+            Coleccion.Add(new TiendasXAML("Tienda 1", "Alamcen 1", "Avenida 1"));
+            Coleccion.Add(new TiendasXAML("Tienda 2", "Almacen 2", "Avenida 2"));
+
+        }
+        private void PrincipalPaginaVM_EventoResizarVentana(object sender, Window e)
+        {
+            AltoGridDatos = e.Height - 220;
+
+        }
+
+        public double AltoGridDatos
+        {
+            get { return this.altoGridDatos; }
+            set { SetValue(ref this.altoGridDatos, value); }
+        }
+
+    }
+}
