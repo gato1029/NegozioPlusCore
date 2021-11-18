393f62356dcde9eba1ce154018335da17643a257
Kenny <kennymorocco@gmail.com>
2021-11-17 19:34:42 -0500

    Se cargo el proyecto actualizado

---
+++ NegozioPlusCore/MVVM/Principal/PrincipalPagina.xaml.cs
@@ -0,0 +1,34 @@
+﻿using NegozioPlusCore.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NegozioPlusCore.MVVM.Principal
+{
+    /// <summary>
+    /// Lógica de interacción para PrincipalPagina.xaml
+    /// </summary>
+    public partial class PrincipalPagina : Window
+    {
+        public PrincipalPagina()
+        {
+            InitializeComponent();
+            ServiceLocator.Instance.RegisterService<PrincipalPagina>(this);
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NegozioPlusCore/Utilitarios/ServiceLocator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegozioPlusCore.Utilitarios
+{
+    class ServiceLocator
+    {
+        public static ServiceLocator Instance => _instance ?? (_instance = new ServiceLocator());
+        private static ServiceLocator _instance;
+
+        private readonly Dictionary<Type, object> _services;
+
+        public ServiceLocator()
+        {
+            _services = new Dictionary<Type, object>();
+        }
+        public bool ExistService<T>()
+        {
+            var type = typeof(T);
+            if (_services.ContainsKey(type))
+            {
+                return true;
+            }
+            return false;
+        }
+        public void RegisterService<T>(T service)
+        {
+            var type = typeof(T);
+            if (!_services.ContainsKey(type))
+            {
+                _services.Add(type, service);
+            }                                                   
+        }
+        public void ReplaceService<T>(T service)
+        {
+            var type = typeof(T);
+            if (_services.ContainsKey(type))
+            {
+                _services[type] = service;                
+            }
+        }
+        public void RemoveService<T>()
+        {
+            var type = typeof(T);
+            if (_services.ContainsKey(type))
+            {
+                _services.Remove(type);
+            }
+        }
+        public T GetService<T>()
+        {
+            var type = typeof(T);
+            if (!_services.TryGetValue(type, out var service))
+            {
+                throw new Exception($"Service {type} not found");
+            }
+            return (T)service;
+        }
+    }
+}
